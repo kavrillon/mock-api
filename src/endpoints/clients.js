99efc22238fc1c7c964ef1b2a413e633b26bf3fa
Kevin Avrillon <kevin@avrillon.me>
2019-10-09 22:33:00 +0200

    fix(client): messages to object

--- src/endpoints/clients.js
+++ src/endpoints/clients.js
@@ -29,62 +29,60 @@ export function generate(destination) {
         locales: locales,
         defaultLocale: 'fr',
         headerLinks,
-        messages: [
-          {
-            fr: {
-              translation: {
-                home: {
-                  title: 'Page de recherche',
-                  search: 'Trouver un point de vente',
-                  more: 'Afficher plus de résultats'
-                },
-                business: {
-                  latestReviews: 'Derniers avis',
-                  map: 'Carte'
-                }
+        messages: {
+          fr: {
+            translation: {
+              home: {
+                title: 'Page de recherche',
+                search: 'Trouver un point de vente',
+                more: 'Afficher plus de résultats'
+              },
+              business: {
+                latestReviews: 'Derniers avis',
+                map: 'Carte'
               }
-            },
-            en: {
-              translation: {
-                home: {
-                  title: 'Search page',
-                  search: 'Find a shop',
-                  more: 'Show more results'
-                },
-                business: {
-                  latestReviews: 'Latest reviews',
-                  map: 'Map'
-                }
+            }
+          },
+          en: {
+            translation: {
+              home: {
+                title: 'Search page',
+                search: 'Find a shop',
+                more: 'Show more results'
+              },
+              business: {
+                latestReviews: 'Latest reviews',
+                map: 'Map'
               }
-            },
-            es: {
-              translation: {
-                home: {
-                  title: 'Página de búsqueda',
-                  search: 'Encuentra una tienda',
-                  more: 'Mostrar más resultados'
-                },
-                business: {
-                  latestReviews: 'Últimas revisiones',
-                  map: 'Mapa'
-                }
+            }
+          },
+          es: {
+            translation: {
+              home: {
+                title: 'Página de búsqueda',
+                search: 'Encuentra una tienda',
+                more: 'Mostrar más resultados'
+              },
+              business: {
+                latestReviews: 'Últimas revisiones',
+                map: 'Mapa'
               }
-            },
-            ar: {
-              translation: {
-                home: {
-                  title: 'صفحة البحث',
-                  search: 'العثور على نقطة البيع',
-                  more: 'عرض المزيد من النتائج'
-                },
-                business: {
-                  latestReviews: 'أحدث الاستعراضات',
-                  map: 'خريطة'
-                }
+            }
+          },
+          ar: {
+            translation: {
+              home: {
+                title: 'صفحة البحث',
+                search: 'العثور على نقطة البيع',
+                more: 'عرض المزيد من النتائج'
+              },
+              business: {
+                latestReviews: 'أحدث الاستعراضات',
+                map: 'خريطة'
               }
             }
           }
-        ]
+        }
       };
     });
 
