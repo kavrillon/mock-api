d4081f074ed686424d0f7e1e53dfff96a6c850c6
Kevin Avrillon <kevin@avrillon.me>
2019-10-16 14:34:41 +0200

    feat(client): remove useless url

--- src/endpoints/clients.js
+++ src/endpoints/clients.js
@@ -40,7 +40,6 @@ export function generate(destination) {
 
       return {
         id,
-        url: 'https://gator-lpe.netlify.com',
         index: 'test',
         name: brand,
         slug: slugify(brand, SLUGIFY_CONF),
